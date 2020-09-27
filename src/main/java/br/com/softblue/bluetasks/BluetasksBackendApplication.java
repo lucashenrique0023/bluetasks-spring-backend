f157a69f5079b597d72531da2d7ab7e2628f93f1
detectiverust93 <lucas.henrique0023@gmail.com>
2020-09-27 13:55:12 -0300

    Expondo ID das taferas no web service.
    Passando parametros de configuracao para o Spring Data Rest.

--- src/main/java/br/com/softblue/bluetasks/BluetasksBackendApplication.java
+++ src/main/java/br/com/softblue/bluetasks/BluetasksBackendApplication.java
@@ -2,12 +2,22 @@ package br.com.softblue.bluetasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import br.com.softblue.bluetasks.domain.task.Task;
 
 @SpringBootApplication
-public class BluetasksBackendApplication {
+public class BluetasksBackendApplication implements RepositoryRestConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BluetasksBackendApplication.class, args);
+	}
+	
+	// Poderiamos criar uma nova classe anotar com @Configure e implementar a configuracao la.
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Task.class);
 	}
 
 }
