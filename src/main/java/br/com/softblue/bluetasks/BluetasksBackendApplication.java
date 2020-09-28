95a6ea5bc7a034ef0ab257fb9f431f7a9d77de2f
detectiverust93 <lucas.henrique0023@gmail.com>
2020-09-28 17:47:52 -0300

    Configurando o processo de validação para retornar os erros corretamente
    Configuramos a aplicacao para retornar os errors tratados em forma de
    array ao inves de stacktrace ou mensagens internas do java.

--- src/main/java/br/com/softblue/bluetasks/BluetasksBackendApplication.java
+++ src/main/java/br/com/softblue/bluetasks/BluetasksBackendApplication.java
@@ -2,8 +2,12 @@ package br.com.softblue.bluetasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import br.com.softblue.bluetasks.domain.task.Task;
 
@@ -18,6 +22,20 @@ public class BluetasksBackendApplication implements RepositoryRestConfigurer {
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 		config.exposeIdsFor(Task.class);
+	}
+	
+	@Bean
+	public Validator validator() {
+		return new LocalValidatorFactoryBean();
+	}
+	
+	// Configurar Eventos de Validacao (Momento em que a validacao vai ser executada)
+	// As menssagens de erro de validacao ja vao voltar tratadas em um array de errors(sem stacktrace, etc..)
+	@Override
+	public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+		Validator validator = validator();
+		validatingListener.addValidator("beforeCreate", validator);
+		validatingListener.addValidator("beforeSave", validator);
 	}
 
 }
